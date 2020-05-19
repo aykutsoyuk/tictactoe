5ed54b6d4563364d6aa8d447669369ec63426bae
Aykut Söyük <aykutsoyuk@gmail.com>
2020-05-19 15:22:19 +0300

    startBtnAddedStyleProblemsFixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cells = document.querySelectorAll(".cell")
 
 const easyBtn = document.querySelector("#easyBtn")
 const hardBtn = document.querySelector("#hardBtn")
-const bounce = document.getElementsByClassName("bounce")
+
 
 const winning = [
     [0,1,2],
@@ -18,15 +18,21 @@ const winning = [
     [2,5,8]
 ]
 
-
-hardBtn.addEventListener('click', function(){
+//fonksiyonlar iptal olmadığı için önce hangisi başlarsa o devam ediyon
+hardBtn.addEventListener('click', function(e){
     hardBtn.classList.add("active")
     easyBtn.classList.remove("active")
-})
-easyBtn.addEventListener('click', function(){
+    e.preventDefault()
+    hardGameBegin()
+    
+}, false)
+easyBtn.addEventListener('click', function(e){
     easyBtn.classList.add("active")
     hardBtn.classList.remove("active")
-})
+    e.preventDefault()
+    easyGameBegin()
+    
+}, false)
 
 restartBtn.addEventListener('click', function(){
         cells.forEach(cell => cell.textContent = "")
@@ -34,20 +40,44 @@ restartBtn.addEventListener('click', function(){
         document.querySelector("#result").innerHTML = ""
         
     })
-////////////////////////////
-function compThinking() {
-    bounce.style.removeProperty("display")
-}   
+//////////////////////////// compthinking animasyon
+// function compThinking() {
+//     document.getElementsByClassName("bounce").style.display = ""
+// }   
 
-    cells.forEach(cell => cell.addEventListener('click', function (e) {
-        if (e.target.textContent === "") {
-            cell.innerHTML = 'X'  
-            setTimeout(compChoiceHard, 300)
-            setTimeout(compThinking, 100)  
-        }
-        
-        
-    }))
+
+
+    function hardGameBegin() {
+        cells.forEach(cell => cell.addEventListener('click', function (e) {
+            if (e.target.textContent === "") {
+                cell.innerHTML = 'X'  
+                setTimeout(compChoiceHard, 100)
+                // setTimeout(compThinking, 100)  
+            }
+            
+            
+        }))
+    
+    }
+
+
+    function easyGameBegin() {
+        cells.forEach(cell => cell.addEventListener('click', function (e) {
+            if (e.target.textContent === "") {
+                cell.innerHTML = 'X'  
+                setTimeout(compChoiceEasy, 100)
+                // setTimeout(compThinking, 100)  
+            }
+            
+            
+        }))
+    
+    }
+
+
+
+
+
 /////////////////////////////////////////
 
 function userWins() {
@@ -188,4 +218,54 @@ function compChoiceHard() {
     result()
 }
 
+function compChoiceEasy() {
+    const randomCell = cells[Math.floor(Math.random() * 9)]
+    if (((cells[1].textContent === cells[2].textContent && cells[2].textContent === "O") || (cells[4].textContent === cells[8].textContent && cells[8].textContent === "O") || (cells[3].textContent === cells[6].textContent && cells[6].textContent === "O")) && cells[0].textContent === "") {
+        cells[0].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[2].textContent && cells[2].textContent === "O") || (cells[4].textContent === cells[7].textContent && cells[7].textContent === "O")) && cells[1].textContent === "") {
+        cells[1].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[1].textContent && cells[1].textContent === "O") || (cells[4].textContent === cells[6].textContent && cells[6].textContent === "O") || (cells[5].textContent === cells[8].textContent && cells[8].textContent === "O")) && cells[2].textContent === "") {
+        cells[2].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[6].textContent && cells[6].textContent === "O") || (cells[4].textContent === cells[5].textContent && cells[5].textContent === "O")) && cells[3].textContent === "") {
+        cells[3].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[8].textContent && cells[8].textContent === "O") || (cells[2].textContent === cells[6].textContent && cells[6].textContent === "O") || (cells[1].textContent === cells[7].textContent && cells[7].textContent === "O") || (cells[3].textContent === cells[5].textContent && cells[5].textContent === "O")) && cells[4].textContent === "") {
+        cells[4].innerHTML = "O"
+    } else if (((cells[2].textContent === cells[8].textContent && cells[8].textContent === "O") || (cells[3].textContent === cells[4].textContent && cells[4].textContent === "O")) && cells[5].textContent === "") {
+        cells[5].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[3].textContent && cells[3].textContent === "O") || (cells[2].textContent === cells[4].textContent && cells[4].textContent === "O") || (cells[7].textContent === cells[8].textContent && cells[8].textContent === "O")) && cells[6].textContent === "") {
+        cells[6].innerHTML = "O"
+    } else if (((cells[1].textContent === cells[4].textContent && cells[4].textContent === "O") || (cells[6].textContent === cells[8].textContent && cells[8].textContent === "O")) && cells[7].textContent === "") {
+        cells[7].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[4].textContent && cells[4].textContent === "O") || (cells[2].textContent === cells[5].textContent && cells[5].textContent === "O") || (cells[6].textContent === cells[7].textContent && cells[7].textContent === "O")) && cells[8].textContent === "") {
+        cells[8].innerHTML = "O"
+    } else if (((cells[1].textContent === cells[2].textContent && cells[2].textContent === "X") || (cells[4].textContent === cells[8].textContent && cells[8].textContent === "X") || (cells[3].textContent === cells[6].textContent && cells[6].textContent === "X")) && cells[0].textContent === "") {
+        cells[0].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[2].textContent && cells[2].textContent === "X") || (cells[4].textContent === cells[7].textContent && cells[7].textContent === "X")) && cells[1].textContent === "") {
+        cells[1].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[1].textContent && cells[1].textContent === "X") || (cells[4].textContent === cells[6].textContent && cells[6].textContent === "X") || (cells[5].textContent === cells[8].textContent && cells[8].textContent === "X")) && cells[2].textContent === "") {
+        cells[2].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[6].textContent && cells[6].textContent === "X") || (cells[4].textContent === cells[5].textContent && cells[5].textContent === "X")) && cells[3].textContent === "") {
+        cells[3].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[8].textContent && cells[8].textContent === "X") || (cells[2].textContent === cells[6].textContent && cells[6].textContent === "X") || (cells[1].textContent === cells[7].textContent && cells[7].textContent === "X") || (cells[3].textContent === cells[5].textContent && cells[5].textContent === "X")) && cells[4].textContent === "") {
+        cells[4].innerHTML = "O"
+    } else if (((cells[2].textContent === cells[8].textContent && cells[8].textContent === "X") || (cells[3].textContent === cells[4].textContent && cells[4].textContent === "X")) && cells[5].textContent === "") {
+        cells[5].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[3].textContent && cells[3].textContent === "X") || (cells[2].textContent === cells[4].textContent && cells[4].textContent === "X") || (cells[7].textContent === cells[8].textContent && cells[8].textContent === "X")) && cells[6].textContent === "") {
+        cells[6].innerHTML = "O"
+    } else if (((cells[1].textContent === cells[4].textContent && cells[4].textContent === "X") || (cells[6].textContent === cells[8].textContent && cells[8].textContent === "X")) && cells[7].textContent === "") {
+        cells[7].innerHTML = "O"
+    } else if (((cells[0].textContent === cells[4].textContent && cells[4].textContent === "X") || (cells[2].textContent === cells[5].textContent && cells[5].textContent === "X") || (cells[6].textContent === cells[7].textContent && cells[7].textContent === "X")) && cells[8].textContent === "") {
+        cells[8].innerHTML = "O"
+    } else if (randomCell.textContent === ""){
+        randomCell.innerHTML = "O"
+    } else { 
+        for (i = 0 ; i <= 8 ; i++) {
+            if (cells[i].textContent === "") {
+                cells[i].innerHTML = "O"
+                break
+            }
+        }
 
+    }
+    result()
+}
